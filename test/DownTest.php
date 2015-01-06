6075dcb70ed16b1dd1f4f170045456bca268f770
Niklas Keller <me@kelunik.com>
2015-01-06 13:09:12 +0100

    Fix DownTest

--- test/DownTest.php
+++ test/DownTest.php
@@ -7,12 +7,17 @@ use Amp\NativeReactor;
 class DownTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @test
-	 * @expectedException \Amp\Redis\RedisException
 	 */
 	function ping () {
 		(new NativeReactor)->run(function ($reactor) {
 			$redis = new Redis($reactor, ["host" => "127.0.0.1:25325", "password" => "secret"]);
-			yield $redis->ping();
+
+			try {
+				yield $redis->ping();
+				$this->fail("no exception thrown");
+			} catch (\Exception $e) {
+				$this->assertInstanceOf("\\Amp\\Redis\\RedisException", $e);
+			}
 		});
 	}
 }
