95e5092bf75637a1e3d8f2d698ef0ed284c4d3ff
Niklas Keller <me@kelunik.com>
2015-01-25 19:28:05 +0100

    goto all the things!

--- lib/RespParser.php
+++ lib/RespParser.php
@@ -29,115 +29,109 @@ class RespParser {
 	public function append ($str) {
 		$this->buffer .= $str;
 
-		while ($this->tryParse()) ;
-	}
+		start: {
+			$type = $this->buffer[0];
+			$pos = strpos($this->buffer, self::CRLF);
 
-	private function tryParse () {
-		if (strlen($this->buffer) === 0) {
-			return false;
-		}
+			if ($pos === false) {
+				return;
+			}
 
-		$type = $this->buffer[0];
-		$pos = strpos($this->buffer, self::CRLF);
-
-		if ($pos === false) {
-			return false;
-		}
-
-		switch ($type) {
-			case self::TYPE_SIMPLE_STRING:
-			case self::TYPE_INTEGER:
-			case self::TYPE_ARRAY:
-			case self::TYPE_ERROR:
-				$payload = substr($this->buffer, 1, $pos - 1);
-				$remove = $pos + 2;
-				break;
-
-			case self::TYPE_BULK_STRING:
-				$length = (int) substr($this->buffer, 1, $pos);
-
-				if($length === -1) {
-					$payload = null;
+			switch ($type) {
+				case self::TYPE_SIMPLE_STRING:
+				case self::TYPE_INTEGER:
+				case self::TYPE_ARRAY:
+				case self::TYPE_ERROR:
+					$payload = substr($this->buffer, 1, $pos - 1);
 					$remove = $pos + 2;
-				} else {
-					if (strlen($this->buffer) < $pos + $length + 4) {
-						return false;
+					break;
+
+				case self::TYPE_BULK_STRING:
+					$length = (int) substr($this->buffer, 1, $pos);
+
+					if($length === -1) {
+						$payload = null;
+						$remove = $pos + 2;
+					} else {
+						if (strlen($this->buffer) < $pos + $length + 4) {
+							return;
+						}
+
+						$payload = substr($this->buffer, $pos + 2, $length);
+						$remove = $pos + $length + 4;
 					}
 
-					$payload = substr($this->buffer, $pos + 2, $length);
-					$remove = $pos + $length + 4;
-				}
+					break;
 
-				break;
+				default:
+					throw new ParserException (
+						sprintf("unknown resp data type: %s", $type)
+					);
+			}
 
-			default:
-				throw new ParserException (
-					sprintf("unknown resp data type: %s", $type)
-				);
+			$this->buffer = substr($this->buffer, $remove);
+
+			switch ($type) {
+				case self::TYPE_INTEGER:
+				case self::TYPE_ARRAY:
+					$payload = intval($payload);
+					break;
+
+				case self::TYPE_ERROR:
+					$payload = new RedisException($payload);
+					break;
+
+				default:
+					break;
+			}
 		}
 
-		$this->buffer = substr($this->buffer, $remove);
-
-		switch ($type) {
-			case self::TYPE_INTEGER:
-			case self::TYPE_ARRAY:
-				$payload = intval($payload);
-				break;
-
-			case self::TYPE_ERROR:
-				$payload = new RedisException($payload);
-				break;
-
-			default:
-				break;
-		}
-
-		$this->onRespParsed($type, $payload);
-
-		return true;
-	}
-
-	private function onRespParsed ($type, $payload) {
-		if ($this->currentResponse !== null) { // extend array response
-			if ($type === self::TYPE_ARRAY) {
-				if ($payload >= 0) {
-					$this->arraySizes[] = $this->currentSize;
-					$this->arrayStack[] = &$this->currentResponse;
-					$this->currentSize = $payload + 1;
-					$this->currentResponse[] = [];
-					$this->currentResponse = &$this->currentResponse[sizeof($this->currentResponse) - 1];
+		complete: {
+			if ($this->currentResponse !== null) { // extend array response
+				if ($type === self::TYPE_ARRAY) {
+					if ($payload >= 0) {
+						$this->arraySizes[] = $this->currentSize;
+						$this->arrayStack[] = &$this->currentResponse;
+						$this->currentSize = $payload + 1;
+						$this->currentResponse[] = [];
+						$this->currentResponse = &$this->currentResponse[sizeof($this->currentResponse) - 1];
+					} else {
+						$this->currentResponse[] = null;
+					}
 				} else {
-					$this->currentResponse[] = null;
-				}
-			} else {
-				$this->currentResponse[] = $payload;
-			}
-
-			while (--$this->currentSize === 0) {
-				if (sizeof($this->arrayStack) === 0) {
-					call_user_func($this->responseCallback, $this->currentResponse);
-					$this->currentResponse = null;
-					return;
+					$this->currentResponse[] = $payload;
 				}
 
-				// index doesn't start at 0 :(
-				end($this->arrayStack);
-				$key = key($this->arrayStack);
-				$this->currentResponse = &$this->arrayStack[$key];
-				$this->currentSize = array_pop($this->arraySizes);
-				unset($this->arrayStack[$key]);
+				while (--$this->currentSize === 0) {
+					if (sizeof($this->arrayStack) === 0) {
+						call_user_func($this->responseCallback, $this->currentResponse);
+						$this->currentResponse = null;
+						return;
+					}
+
+					// index doesn't start at 0 :(
+					end($this->arrayStack);
+					$key = key($this->arrayStack);
+					$this->currentResponse = &$this->arrayStack[$key];
+					$this->currentSize = array_pop($this->arraySizes);
+					unset($this->arrayStack[$key]);
+				}
+			} else if ($type === self::TYPE_ARRAY) { // start new array response
+				if ($payload > 0) {
+					$this->currentSize = $payload;
+					$this->arrayStack = $this->arraySizes = $this->currentResponse = [];
+				} else if ($payload === 0) {
+					call_user_func($this->responseCallback, []);
+				} else {
+					call_user_func($this->responseCallback, null);
+				}
+			} else { // single data type response
+				call_user_func($this->responseCallback, $payload);
 			}
-		} else if ($type === self::TYPE_ARRAY) { // start new array response
-			if ($payload > 0) {
-				$this->currentSize = $payload;
-				$this->arrayStack = $this->arraySizes = $this->currentResponse = [];
-			} else if ($payload === 0) {
-				call_user_func($this->responseCallback, []);
-			} else {
-				call_user_func($this->responseCallback, null);
+
+			if(isset($this->buffer[0])) {
+				goto start;
 			}
-		} else { // single data type response
-			call_user_func($this->responseCallback, $payload);
 		}
 	}
 }
