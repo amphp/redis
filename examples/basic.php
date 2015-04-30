70ff3349e79bb23f1c615e685b6ac74618e50f14
Niklas Keller <me@kelunik.com>
2015-04-30 20:45:53 +0200

    Add basic usage example

---
+++ examples/basic.php
@@ -0,0 +1,10 @@
+<?php
+
+Amp\run(function () {
+    $client = new \Amp\Redis\Client("tcp://localhost:6379");
+
+    yield $client->set("foo", "21");
+    $result = yield $client->incr("foo", 21);
+
+    var_dump($result); // int(42)
+});
