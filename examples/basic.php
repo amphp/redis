30073492c511632aa5c9605f1d8627d99f31fc49
Niklas Keller <me@kelunik.com>
2017-03-01 15:51:04 +0100

    Add blpop example, fix basic example

--- examples/basic.php
+++ examples/basic.php
@@ -1,10 +1,14 @@
 <?php
 
+require __DIR__ . "/../vendor/autoload.php";
+
 Amp\run(function () {
-    $client = new \Amp\Redis\Client("tcp://localhost:6379");
+    $client = new Amp\Redis\Client("tcp://localhost:6379");
 
     yield $client->set("foo", "21");
     $result = yield $client->incr("foo", 21);
 
     var_dump($result); // int(42)
+
+    Amp\stop();
 });
