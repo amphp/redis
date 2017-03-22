19d7bc879ea0a2d8647e4a2d8ffe5b867e57dda9
ekinhbayar <ekin@coproductivity.com>
2017-03-22 17:48:23 +0300

    Update PHPUnit to ^6.0

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -2,7 +2,9 @@
 
 namespace Amp\Redis;
 
-class ParserTest extends \PHPUnit_Framework_TestCase {
+use PHPUnit\Framework\TestCase;
+
+class ParserTest extends TestCase {
     /**
      * @test
      */
