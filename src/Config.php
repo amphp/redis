a3656730c33bcf2b4fa712fe4c399d5f883625cb
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2019-09-09 19:44:55 +0200

    Add connection config

---
+++ src/Config.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Amp\Redis;
+
+use function League\Uri\parse;
+
+final class Config
+{
+    public const DEFAULT_HOST = 'localhost';
+    public const DEFAULT_PORT = '6379';
+
+    /**
+     * @param string $uri
+     *
+     * @return self
+     *
+     * @throws RedisException
+     */
+    public static function fromUri(string $uri): self
+    {
+        if (\stripos($uri, 'tcp://') !== 0 && \stripos($uri, 'unix://') !== 0 && \stripos($uri, 'redis://') !== 0) {
+            throw new RedisException('Invalid redis configuration URI, must start with tcp://, unix:// or redis://');
+        }
+
+        return new self($uri);
+    }
+
+    /** @var string */
+    private $uri;
+    /** @var string */
+    private $password = '';
+    /** @var int */
+    private $database = 0;
+    /** @var int */
+    private $timeout = 5000;
+
+    /**
+     * @param string $uri
+     *
+     * @throws RedisException
+     */
+    public function __construct(string $uri)
+    {
+        $this->applyUri($uri);
+    }
+
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    public function getTimeout(): int
+    {
+        return $this->timeout;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function hasPassword(): bool
+    {
+        return $this->password !== '';
+    }
+
+    public function getDatabase(): int
+    {
+        return $this->database;
+    }
+
+    /**
+     * When using the "redis" schemes the URI is parsed according to the rules defined by the provisional registration
+     * documents approved by IANA. If the URI has a password in its "user-information" part or a database number in the
+     * "path" part these values override the values of "password" / "database" if they are present in the "query" part.
+     *
+     * @link http://www.iana.org/assignments/uri-schemes/prov/redis
+     *
+     * @param string $uri URI string.
+     *
+     * @throws RedisException
+     */
+    private function applyUri(string $uri): void
+    {
+        if ($uri === 'redis://') {
+            $uri = 'redis://' . self::DEFAULT_HOST . ':' . self::DEFAULT_PORT;
+        }
+
+        try {
+            $parsedUri = parse($uri);
+        } catch (\Exception $exception) {
+            throw new RedisException('Invalid redis configuration URI: ' . $uri);
+        }
+
+        \parse_str($parsedUri['query'] ?? '', $query);
+
+        switch (\strtolower($parsedUri['scheme'])) {
+            case 'tcp':
+                $this->uri = 'tcp://' . \strtolower($parsedUri['host']) . ':' . (int) $parsedUri['port'];
+                $this->database = (int) ($query['database'] ?? $query['db'] ?? 0);
+                $this->password = $query['password'] ?? $query['pass'] ?? '';
+
+                break;
+
+            case 'unix':
+                $this->uri = 'unix://' . $parsedUri['path'];
+                $this->database = (int) ($query['database'] ?? $query['db'] ?? 0);
+                $this->password = $query['password'] ?? $query['pass'] ?? '';
+
+                break;
+
+            case 'redis':
+                $host = \strtolower($parsedUri['host'] ?? self::DEFAULT_HOST);
+                $port = (int) ($parsedUri['port'] ?? self::DEFAULT_PORT);
+
+                if ($host === '') {
+                    $host = self::DEFAULT_HOST;
+                }
+
+                if ($port === 0) {
+                    $port = self::DEFAULT_PORT;
+                }
+
+                $this->uri = 'tcp://' . $host . ':' . $port;
+
+                if ($parsedUri['path'] !== '/') {
+                    $this->database = (int) \ltrim($parsedUri['path'], '/');
+                } else {
+                    $this->database = (int) ($query['db'] ?? 0);
+                }
+
+                if (isset($parsedUri['pass']) && $parsedUri['pass'] !== '') {
+                    $this->password = $parsedUri['pass'];
+                } else {
+                    $this->password = $query['password'] ?? '';
+                }
+
+                break;
+        }
+
+        $this->timeout = (int) ($query['timeout'] ?? $this->timeout);
+    }
+}
---
+++ test/ConfigTest.php
@@ -0,0 +1,48 @@
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+namespace Amp\Redis;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test(
+        string $uri,
+        string $expectedUri,
+        int $expectedDatabase,
+        int $expectedTimeout,
+        string $expectedPassword
+    ): void {
+        $config = Config::fromUri($uri);
+
+        self::assertSame($expectedUri, $config->getUri());
+        self::assertSame($expectedDatabase, $config->getDatabase());
+        self::assertSame($expectedTimeout, $config->getTimeout());
+        self::assertSame($expectedPassword, $config->getPassword());
+
+        if ($expectedPassword === '') {
+            self::assertFalse($config->hasPassword());
+        } else {
+            self::assertTrue($config->hasPassword());
+        }
+    }
+
+    public function provideData(): array
+    {
+        return [
+            ['tcp://localhost:6379', 'tcp://localhost:6379', 0, 5000, ''],
+            ['tcp://localhost:6379?database=1', 'tcp://localhost:6379', 1, 5000, ''],
+            ['tcp://localhost:6379?database=1&pass=foobar', 'tcp://localhost:6379', 1, 5000, 'foobar'],
+            ['redis://localhost:6379', 'tcp://localhost:6379', 0, 5000, ''],
+            ['redis://:secret@localhost:6379', 'tcp://localhost:6379', 0, 5000, 'secret'],
+            ['redis://:secret@localhost:6379/3', 'tcp://localhost:6379', 3, 5000, 'secret'],
+            ['redis://:secret@:6379/3?timeout=10000', 'tcp://localhost:6379', 3, 10000, 'secret'],
+            ['redis://:secret@foobar/3?timeout=10000', 'tcp://foobar:6379', 3, 10000, 'secret'],
+            ['unix:///run/redis.sock', 'unix:///run/redis.sock', 0, 5000, ''],
+            ['unix:///run/redis.sock?db=2&password=123', 'unix:///run/redis.sock', 2, 5000, '123'],
+        ];
+    }
+}
