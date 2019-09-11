83644f614f5e97c54afd7a4b7d1cc45478bb70cf
Aaron Piotrowski <aaron@trowski.com>
2019-09-10 23:20:12 -0500

    Add ability to inject Connector

--- src/RemoteExecutorFactory.php
+++ src/RemoteExecutorFactory.php
@@ -2,17 +2,21 @@
 
 namespace Amp\Redis;
 
+use Amp\Socket;
+
 final class RemoteExecutorFactory implements QueryExecutorFactory
 {
     private $config;
+    private $connector;
 
-    public function __construct(Config $config)
+    public function __construct(Config $config, ?Socket\Connector $connector = null)
     {
         $this->config = $config;
+        $this->connector = $connector ?? Socket\connector();
     }
 
     public function createQueryExecutor(): QueryExecutor
     {
-        return new RemoteExecutor($this->config);
+        return new RemoteExecutor($this->config, $this->connector);
     }
 }
