035061e29f90200af257bd9f72439c86f40e557b
Niklas Keller <me@kelunik.com>
2023-08-19 19:34:49 +0200

    Add missing final
    Fixes #83.

--- src/Connection/SocketRedisConnector.php
+++ src/Connection/SocketRedisConnector.php
@@ -10,7 +10,7 @@ use Amp\Socket;
 use Amp\Socket\ConnectContext;
 use Amp\Socket\SocketConnector;
 
-class SocketRedisConnector implements RedisConnector
+final class SocketRedisConnector implements RedisConnector
 {
     public function __construct(
         private readonly ?SocketConnector $connector = null,
